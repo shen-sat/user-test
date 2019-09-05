2236080f57510e1fb05d31e04a8f0289d4410ee5
Shen Sat <shensat@shens-mbp.home>
2019-09-05 21:26:51 +0100

    Add Users Controller, start re-configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'users#new'
+  get 'users/create'
   get 'welcome/index'
 
-  root 'welcome#index'
+  # root 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
